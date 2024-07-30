fe1cdf977e2b162545e2fc9806f61fc3cf0907b1
bayanjd <bayanjallad535@gmail.com>
2024-07-30 13:32:18 -0700

    first task commit

---
+++ lib/core/resources/style.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'color.dart';
+
+const TextStyle titelTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 14,
+  color: titelColor,
+);
+
+const TextStyle subtitleTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+  color: subtitleColor,
+);
+
+const TextStyle trailaingTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+  color: titelColor,
+);
+
+const TextStyle trailingDoneTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+  color: mediumGreenColor,
+);
+
+const TextStyle trailingCencelTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 12,
+  color: mediumRedColor,
+);
+
+const TextStyle onBordingTitelTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 24,
+  color: titelColor,
+);
+
+const TextStyle onBordingSubTitleTextStyle = TextStyle(
+  fontWeight: FontWeight.w500,
+  fontSize: 24,
+  color: subtitleOnBordingColor,
+);
---
+++ lib/core/widgets_App/listTile_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// ignore: non_constant_identifier_names
+Widget ListTileCustom(
+    Widget title,
+    TextStyle titelTextStyle,
+    Widget subtitle,
+    Widget leading,
+    Widget trailing,
+    TextStyle subTitleTextStyle,
+    TextStyle leadingAndTrailingTextStyle) {
+  return ListTile(
+    title: title,
+    titleTextStyle: titelTextStyle,
+    subtitle: subtitle,
+    subtitleTextStyle: subTitleTextStyle,
+    leading: leading,
+    trailing: trailing,
+    leadingAndTrailingTextStyle: leadingAndTrailingTextStyle,
+  );
+}
